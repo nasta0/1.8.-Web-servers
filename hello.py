bb8b2e7503060f2f3d5cfa7d0747d12de5cd51c3
nasta0 <51378442+nasta0@users.noreply.github.com>
2019-11-18 20:17:41 +0300

    Create hello.py

---
+++ hello.py
@@ -0,0 +1,8 @@
+def app(environ, start_response):
+	body = [bytes(i + '\n', 'ascii') for i in environ['QUERY_STRING'].split('&')]
+	status = '200 OK'
+	headers = [('Content-Type','text/plain')]
+	
+	start_response(status, headers)
+
+	return body
