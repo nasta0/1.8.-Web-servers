d25bcde42b8e0bf3e645fad85424a34769107c5b
nasta0 <51378442+nasta0@users.noreply.github.com>
2019-12-03 20:22:29 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,4 +1,5 @@
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
-sudo ln -fs /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
+#sudo ln -fs /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
 sudo /etc/init.d/nginx restart
-sudo /etc/init.d/gunicorn restart
+#sudo /etc/init.d/gunicorn restart
+sudo gunicorn -c /home/box/web/etc/hello.py hello:app
